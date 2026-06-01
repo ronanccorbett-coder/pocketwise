3b8905f79fee37c1b96a84bdbaa26673a053d57b
Ronan-CC <rc30@mahurangi.school.nz>
2026-06-02 10:11:57 +1200

    fix root redirect

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [],
     unoptimized: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/landing',
+        permanent: false,
+      },
+    ];
   },
 };
 module.exports = nextConfig;
