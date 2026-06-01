984172f1f14adddc7c9c760e1b56e3f04f9c449f
Ronan-CC <rc30@mahurangi.school.nz>
2026-06-02 10:28:57 +1200

    add Providers component

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,7 +1,13 @@
+
 "use client";
+
 import { GameProvider } from "@/lib/gameContext";
+
 import { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
+
   return <GameProvider>{children}</GameProvider>;
+
 }
+
